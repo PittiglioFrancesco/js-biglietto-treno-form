adeaa14ac1fa49f12ba5e225f4f34136c066669a
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-06-05 14:56:45 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -8,14 +8,31 @@ document.getElementById("genera").addEventListener("click", function() {
   var scontoType = document.getElementById("age").value;
   document.getElementById("type-fine").innerHTML = scontoType;
 
-  var carrozza = Math.floor(Math.random() * 10) + 1;
+  var carrozza = Math.floor(Math.random() * 9) + 1;
   document.getElementById("carrozza").innerHTML = carrozza;
 
-  var codice = Math.floor(Math.random() * 1000000) + 1;
+  var codice = Math.floor(Math.random() * (100000 - 90000 + 1) ) + 90000;
   document.getElementById("codice").innerHTML = codice;
 
-  var costo = km * 0.5;
-  document.getElementById("prezzo-fine").innerHTML = costo + "€";
+  var costo = km * 0.21;
+  if (scontoType == "Minorenne") {
+
+    costo *= 0.8;
+    costo = costo.toFixed(2);
+    document.getElementById("prezzo-fine").innerHTML = costo + "€";
+
+} else if (scontoType == "Over 65") {
+
+    costo *= 0.6;
+    costo = costo.toFixed(2);
+    document.getElementById("prezzo-fine").innerHTML = costo + "€";
+
+} else {
+
+    costo = costo.toFixed(2);
+    document.getElementById("prezzo-fine").innerHTML = costo + "€";
+
+}
     
 
   });
